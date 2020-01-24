688f891f48a8244cd66adb5ba0aa52bbc4f97708
braswelljr <braswellkennth7@gmail.com>
2020-01-24 12:06:11 +0000

    boat

--- app/views/pages/login.php
+++ app/views/pages/login.php
@@ -15,20 +15,32 @@
             <?php echo $data['title']; ?>
         </div>
 
-        <form>
+        <form class="needs-validation" novalidate>
             <div class="form-group">
                 <label for="email">Email or Username</label>
-                <input type="text" class="form-control form-control-lg" autocomplete>
+                <input type="text" class="form-control form-control-lg" required>
+                <div class="valid-feedback">
+                    Good!
+                </div>
+                <div class="invalid-feedback">
+                    Please provide a valid Username or E-mail!
+                </div>
             </div>
             <div class="form-group">
                 <label for="email">Password</label>
-                <input type="password" class="form-control form-control-lg" autocomplete>
+                <input type="password" class="form-control form-control-lg" required>
+                <div class="valid-feedback">
+                    Good!
+                </div>
+                <div class="invalid-feedback">
+                    Please provide a valid password!
+                </div>
             </div>
-            <button class="btn btn-outline-primary btn-lg btn-block">Login</button>
+            <button class="btn btn-outline-primary btn-lg btn-block" type="submit">Login</button>
         </form>
     </div>
 
-
+    <script src="<?php echo URLROOT; ?>/js/formvalidate.js"></script>
     <script src="<?php echo URLROOT; ?>/js/jquery-3.4.1.slim.min.js"></script>
     <script src="<?php echo URLROOT; ?>/js/popper-1.16.0.min.js"></script>
     <script src="<?php echo URLROOT; ?>/js/bootstrap.js"></script>
