df6651a6d803576e23839f793929d1ff61a9d85c
braswelljr <braswellkennth7@gmail.com>
2020-02-08 08:35:08 +0000

    styling
    Signed-off-by: braswelljr <braswellkennth7@gmail.com>

--- app/views/face/home.php
+++ app/views/face/home.php
@@ -12,16 +12,19 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="<?php echo SITENAME; ?>" content="Homepage">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/bootstrap.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/home.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/next.css">
     <title><?php echo SITENAME; ?></title>
+
 </head>
 <body>
-    <div class="shadow-lg p-3 mb-5 bg-white rounded">
-        <img class="logo" src="<?php echo URLROOT; ?>/logo/sailboat.png" alt="logo"><?php echo SITENAME; ?>
+    <div class="navbar shadow-lg p-3 mb-5 bg-white rounded">
+        <text class="boat"><img class="logo" src="<?php echo URLROOT; ?>/logo/sailboat.png" alt="logo"><text><?php echo SITENAME; ?></text></text>
     </div>
     <div class="container-fluid">
-        <div class="row">
-            <div class="col"></div>
+        <!--<div class="row">
+            <div class="col w-25 p-3" style="color: #45aaf2;">
+                <img src="<?//php echo URLROOT; ?>/logo/sailboat.png" alt="..." class="profile-picture rounded-circle border border-primary rounded-lg">
+            </div>
             <div class="col"></div>
         </div>
     </div>
