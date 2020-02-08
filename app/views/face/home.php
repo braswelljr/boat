585416f586c8d9813ede985470fbbead07dc5e06
braswelljr <braswellkennth7@gmail.com>
2020-02-08 12:30:55 +0000

    Home page update
    Signed-off-by: braswelljr <braswellkennth7@gmail.com>

--- app/views/face/home.php
+++ app/views/face/home.php
@@ -12,21 +12,34 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="<?php echo SITENAME; ?>" content="Homepage">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/bootstrap.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/next.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/home.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/logo/fontawesome/css/all.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/logo/fontawesome/css/fontawesome.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/logo/fontawesome/css/regular.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/logo/fontawesome/css/solid.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/logo/fontawesome/css/svg-with-js.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/logo/fontawesome/css/brands.css">
     <title><?php echo SITENAME; ?></title>
-
 </head>
 <body>
-    <div class="navbar shadow-lg p-3 mb-5 bg-white rounded">
-        <text class="boat"><img class="logo" src="<?php echo URLROOT; ?>/logo/sailboat.png" alt="logo"><text><?php echo SITENAME; ?></text></text>
-    </div>
     <div class="container-fluid">
-        <!--<div class="row">
-            <div class="col w-25 p-3" style="color: #45aaf2;">
-                <img src="<?//php echo URLROOT; ?>/logo/sailboat.png" alt="..." class="profile-picture rounded-circle border border-primary rounded-lg">
+        <div class="row">
+            <div class="col col-auto sidebar" style="">
+                <img class="sailboat side-icons" src="<?php echo URLROOT; ?>/public/logo/sailboat.png" alt="applogo">
+                <i class="fas fa-user"></i>
             </div>
-            <div class="col"></div>
+            <div class="col">
+
+            </div>
         </div>
     </div>
+
+    <script src="<?php echo URLROOT; ?>/public/js/all.js"></script>
+    <script src="<?php echo URLROOT; ?>/public/js/fontawesome.js"></script>
+    <script src="<?php echo URLROOT; ?>/public/js/regular.js"></script>
+    <script src="<?php echo URLROOT; ?>/public/js/solid.js"></script>
+    <script src="<?php echo URLROOT; ?>/public/js/svg-with-js.js"></script>
+    <script src="<?php echo URLROOT; ?>/public/js/brands.js"></script>
 </body>
 </html>
